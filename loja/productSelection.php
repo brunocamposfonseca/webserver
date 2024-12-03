ee39b9cb9beb9aa4a4c695ccf506ff119fc4ad63
brunocamposfonseca <brunocamposfonsecaofc@gmail.com>
2024-12-03 15:56:27 -0300

    loja and connection-db

--- loja/productSelection.php
+++ loja/productSelection.php
@@ -5,7 +5,7 @@
         $resultado = mysqli_query($conexao, $sql);
         if (mysqli_num_rows($resultado) > 0) {
             while ($linha = mysqli_fetch_assoc($resultado)) {
-                echo "<div><h3>" . $linha['nome'] . "</h3><ul><li>Id: " . $linha['id'] . "</li><li>" . "Price: " . $linha['valor'] . "</li><li>" . "Stock: " . $linha['estoque'] . "</li></ul></div>";
+                echo "<div><h3>" . $linha['nome'] . "</h3><ul><li>Id: " . $linha['id'] . "</li><li>" . "Price: $" . number_format($linha['valor'], 2, ".", ',') . "</li><li>" . "Stock: " . $linha['estoque'] . "</li></ul></div>";
             }
         } else {
             echo "Nenhum registro encontrado.";
