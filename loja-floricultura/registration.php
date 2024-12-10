70c990b298db8cf417704ea1ae50cb9fcafec32f
brunocamposfonseca <brunocamposfonsecaofc@gmail.com>
2024-12-10 07:48:16 -0300

    mudança do caminho do style.css

--- loja-floricultura/registration.php
+++ loja-floricultura/registration.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="./css/dashboard.css">
-    <link rel="shortcut icon" href="./images//pen.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="./images/pen.ico" type="image/x-icon">
     <title>Dashboard do Vendedor</title>
 </head>
 <body>
